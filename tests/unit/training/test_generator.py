b4b2d686e3b1f9a5f1c2ca4cb11c9bb1684b29e6
Loic Coyle <loic.coyle@hotmail.fr>
2021-10-22 19:40:08 +0200

    tests: add more generator tests

--- tests/unit/training/test_generator.py
+++ tests/unit/training/test_generator.py
@@ -18,20 +18,31 @@ class TestDataGenerator(TestCase):
         with open(cls.blm_name_file, "r") as fp:
             cls.blm_names = json.load(fp)
         cls.blm_dcum = pd.read_hdf(cls.blm_dcum_file, "data")
-        cls.generator = DataGenerator(
-            cls.sample_file,
-            key="STABLE",
-            batch_size=256,
-            BLM_dcum=cls.blm_dcum,  # type: ignore
-            BLM_names=cls.blm_names,
-            return_dataframe=True,
-        )
         cls.test_dir = Path("test_generator")
         if not cls.test_dir.is_dir():
             cls.test_dir.mkdir(parents=True)
 
+    def setUp(self) -> None:
+        self.generator_pd = DataGenerator(
+            self.sample_file,
+            key="STABLE",
+            batch_size=256,
+            BLM_dcum=self.blm_dcum,  # type: ignore
+            BLM_names=self.blm_names,
+            return_dataframe=True,
+        )
+        self.generator_npy = DataGenerator(
+            self.sample_file,
+            key="STABLE",
+            batch_size=256,
+            BLM_dcum=self.blm_dcum,  # type: ignore
+            BLM_names=self.blm_names,
+            return_dataframe=False,
+            ndim=3,
+        )
+
     def test_init(self):
-        generator = DataGenerator(
+        DataGenerator(
             self.sample_file,
             key="STABLE",
             batch_size=256,
@@ -40,15 +51,46 @@ class TestDataGenerator(TestCase):
         )
 
     def test_len(self):
-        assert len(self.generator) == 1024 / 256
+        assert len(self.generator_pd) == 1024 / 256
+        assert len(self.generator_npy) == 1024 / 256
 
     def test_to_json(self):
         json_file = self.test_dir / "generator_params.json"
-        json_string = self.generator.to_json(json_file)
+        json_string = self.generator_pd.to_json(json_file)
         assert DataGenerator.from_json(json_file).to_json() == json_string
+
+    def test_getitem_len(self):
+        for data, _ in self.generator_pd:
+            assert len(data) == self.generator_pd.batch_size
+        for data, _ in self.generator_npy:
+            assert len(data) == self.generator_npy.batch_size
+
+    def test_getitem_sort(self):
+        data, _ = self.generator_pd[0]
+        assert data.columns.to_list() == self.generator_pd._blm_sorted
+
+    def test_getitem_normalization(self):
+        for data, _ in self.generator_pd:
+            assert (data.max() <= 1).all()
+            assert (data.min() >= 0).all()
+
+        for data, _ in self.generator_npy:
+            assert (data.max() <= 1).all()
+            assert (data.min() >= 0).all()
+
+    def test_getitem_ndim(self):
+        data, _ = self.generator_npy[0]
+        assert data.ndim == self.generator_npy.ndim
+
+    def test_to_dict(self):
+        self.generator_npy.to_dict()
+        self.generator_pd.to_dict()
+
+    def tearDown(self) -> None:
+        # clean closing of open files.
+        del self.generator_pd
+        del self.generator_npy
 
     @classmethod
     def tearDownClass(cls) -> None:
-        # clean closing of open files.
-        del cls.generator
         rmtree(cls.test_dir)
