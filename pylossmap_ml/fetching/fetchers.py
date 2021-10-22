e7dd5a646a734357698a4b0ac9625c0c689b6154
Loic Coyle <loic.coyle@hotmail.fr>
2021-10-22 11:22:24 +0200

    fetching: add saving of dataset info

--- pylossmap_ml/fetching/fetchers.py
+++ pylossmap_ml/fetching/fetchers.py
@@ -1,6 +1,7 @@
 import logging
 from pathlib import Path
 from typing import List
+import json
 
 import pandas as pd
 from pylossmap import BLMDataFetcher
@@ -11,6 +12,18 @@ from ..db import DB
 LOGGER = logging.getLogger(__name__)
 
 
+def save_dataset_info(file_path: Path, **kwargs) -> None:
+    """Save the dataset fetch parameters to file.
+
+    Args:
+        file_path: the file in which to write the fetch parameters.
+        **kwargs: the fetch parameters which will be written to file.
+    """
+    LOGGER.info("Saving dataset info to: %s", file_path)
+    with open(file_path, "w") as file:
+        json.dump(kwargs, file)
+
+
 def fetch(
     t1: pd.Timestamp,
     t2: pd.Timestamp,
@@ -19,6 +32,16 @@ def fetch(
     beam_modes_fetch: List[str] = ["STABLE"],
     BLM_var: str = "LHC.BLMI:LOSS_RS01",
 ) -> None:
+    """Fetch a BLM dataset.
+
+    Args:
+        t1: start timestamp.
+        t2: stop timestamp.
+        destination_dir: folder in which to write the data.
+        beam_modes_fills: fetch data for fills which reach these beam modes.
+        beam_modes_fetch: for the fills that pass, fetch data for this beam mode.
+        BLM_var: the timber BLM var to fetch.
+    """
     destination_dir = Path(destination_dir)
     if not destination_dir.is_dir():
         destination_dir.mkdir(parents=True)
@@ -29,6 +52,15 @@ def fetch(
 
     fills = DB.getLHCFillsByTime(t1, t2, beam_modes=beam_modes_fills, unixtime=True)
     LOGGER.info("Found %i fills.", len(fills))
+    save_dataset_info(
+        destination_dir / ".dataset_info.json",
+        t1=t1,
+        t2=t2,
+        destination_dir=destination_dir,
+        beam_modes_fills=beam_modes_fills,
+        beam_modes_fetch=beam_modes_fetch,
+        BLM_var=BLM_var,
+    )
 
     for fill in tqdm(fills, desc="Fetching data"):
         save_path = (destination_dir / str(fill["fillNumber"])).with_suffix(".h5")
