af5e03447d4b74061a34c5b4acfe36d0f2997fa9
Loic Coyle <loic.coyle@hotmail.fr>
2021-10-22 16:45:53 +0200

    tests: add blm_dcum

--- tests/unit/training/test_generator.py
+++ tests/unit/training/test_generator.py
@@ -2,6 +2,8 @@ import json
 from unittest import TestCase
 from pathlib import Path
 
+import pandas as pd
+
 from pylossmap_ml.training.generator import DataGenerator
 
 
@@ -11,13 +13,16 @@ class TestDataGenerator(TestCase):
         cls.test_data_dir = Path(__file__).parents[2] / "data"
         cls.sample_file = cls.test_data_dir / "test_sample.h5"
         cls.blm_name_file = cls.test_data_dir / "test_sample_blm_names.json"
+        cls.blm_dcum_file = cls.test_data_dir / "blm_dcum.h5"
         with open(cls.blm_name_file, "r") as fp:
             cls.blm_names = json.load(fp)
+        cls.blm_dcum = pd.read_hdf(cls.blm_dcum_file, "data")
 
     def test_init(self):
         generator = DataGenerator(
             self.sample_file,
             key="STABLE",
             batch_size=256,
+            BLM_dcum=self.blm_dcum,  # type: ignore
             BLM_names=self.blm_names,
         )
